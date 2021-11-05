ad7e6ff7f2476363cceae008e0015b6799694db7
DESKTOP-6LBPTGQ\IP3 <trol.witchdoctor@gmail.com>
2021-11-05 15:47:12 +0100

    started implementing AuthenticationFilter

---
+++ PhotoAppApiUsers/src/main/java/com/djordje/photoapp/api/users/security/AuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.djordje.photoapp.api.users.security;
+
+import com.djordje.photoapp.api.users.ui.model.LoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestModel creds = new ObjectMapper().readValue(request.getInputStream(), LoginRequestModel.class);
+
+            return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}

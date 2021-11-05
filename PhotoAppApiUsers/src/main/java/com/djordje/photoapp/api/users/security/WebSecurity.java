e1609a02ce58b5ccfc0655aa339b3758284bd2ee
DESKTOP-6LBPTGQ\IP3 <trol.witchdoctor@gmail.com>
2021-11-05 14:07:11 +0100

    added media types for post mapping

--- PhotoAppApiUsers/src/main/java/com/djordje/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/djordje/photoapp/api/users/security/WebSecurity.java
@@ -1,17 +1,20 @@
 package com.djordje.photoapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").hasIpAddress("192.168.2.81");
+        http.headers().frameOptions().disable();
     }
 }
--- PhotoAppApiUsers/src/main/java/com/djordje/photoapp/api/users/services/impl/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/djordje/photoapp/api/users/services/impl/UsersServiceImpl.java
@@ -6,7 +6,7 @@ import com.djordje.photoapp.api.users.services.UsersService;
 import com.djordje.photoapp.api.users.shared.UsersDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,19 +15,21 @@ import java.util.UUID;
 public class UsersServiceImpl implements UsersService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
-    public UsersServiceImpl(UserRepository userRepository) {
+    public UsersServiceImpl(UserRepository userRepository, BCryptPasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     @Override
     public UsersDto createUser(UsersDto userDetails) {
 
         userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(encoder.encode(userDetails.getPassword()));
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-        userEntity.setEncryptedPassword("test");
 
         userRepository.save(userEntity);
 
